11251b5f42d4aca4efd481f835625902d2bf988b
Luke_YS <yjsmk0902@gmail.com>
2024-05-18 04:12:11 +0900

    :construction_worker: Init: CI/CD 세팅 (#1)

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import os
 from ultralytics import YOLO
 import requests
+from PIL import Image
 
 # Flask 애플리케이션 초기화
 app = Flask(__name__)
@@ -11,8 +12,7 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # YOLOv8 모델 불러오기
-model = YOLO('object-predict.pt')
-model.eval()
+model = YOLO('best.pt')
 
 
 @app.route('/predict', methods=['POST'])
@@ -31,14 +31,15 @@ def predict():
         file.save(file_path)
 
         # 예측 수행
-        results = model(file_path)
+        img = Image.open(file_path)
+        results = model.predict(img)  # 이미지 객체를 사용하여 예측
 
-        # 결과 처리
-        for result in results:
-            result.save_txt(result.txt)
+        # 결과 처리 및 텍스트 파일로 저장
+        result_txt_path = os.path.join(app.config['UPLOAD_FOLDER'], 'result.txt')
+        results[0].save_txt(result_txt_path)  # 첫 번째 결과를 텍스트 파일로 저장
 
         # 텍스트 파일의 내용을 String으로 변환
-        with open('result.txt', 'r') as f:
+        with open(result_txt_path, 'r') as f:
             predictions_txt = f.read()
 
         # 스프링 서버로 예측 결과 전송
@@ -49,6 +50,38 @@ def predict():
             return jsonify({'message': 'Prediction sent successfully', 'predictions': predictions_txt})
         else:
             return jsonify({'error': 'Failed to send prediction to Spring server'}), 500
+
+    return jsonify({'error': 'File upload failed'}), 500
+
+
+@app.route('/predict-test', methods=['POST'])
+def predict_test():
+    # 요청에서 파일 가져오기
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+
+    if file:
+        # 파일 저장
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(file_path)
+
+        # 예측 수행
+        img = Image.open(file_path)
+        results = model.predict(img)  # 이미지 객체를 사용하여 예측
+
+        # 결과 처리 및 텍스트 파일로 저장
+        result_txt_path = os.path.join(app.config['UPLOAD_FOLDER'], 'result.txt')
+        results[0].save_txt(result_txt_path)  # 첫 번째 결과를 텍스트 파일로 저장
+
+        # 텍스트 파일의 내용을 String으로 변환
+        with open(result_txt_path, 'r') as f:
+            predictions_txt = f.read()
+
+        return jsonify({'predictions': predictions_txt})
 
     return jsonify({'error': 'File upload failed'}), 500
 
