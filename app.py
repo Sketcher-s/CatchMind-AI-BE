c1bb5b7d4e2d06f31040c4f3b52bce9cc4a5d520
Luke_YS <yjsmk0902@gmail.com>
2024-05-20 13:48:06 +0900

    :recycle: REFACTOR: 상수 처리 및 시간 로그

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from ultralytics import YOLO
 import requests
 from PIL import Image
 
+import time
+
 # Flask 애플리케이션 초기화
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
@@ -14,16 +16,26 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # YOLOv8 모델 불러오기
 model = YOLO('best.pt')
 
+# 응답 메시지
+ERROR_MESSAGE_NO_FILE = 'No file part'
+ERROR_MESSAGE_NO_SELECTED_FILE = 'No selected file'
+ERROR_MESSAGE_FILE_UPLOAD_FAILED = 'File upload failed'
+SUCCESS_MESSAGE = 'Prediction sent successfully'
+
+# 상수
+RESULT_TEXT_FILE = 'result.txt'
+
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
     # 요청에서 파일 가져오기
     if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
+        return jsonify({'error': ERROR_MESSAGE_NO_FILE}), 400
 
     file = request.files['file']
     if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
+        return jsonify({'error': ERROR_MESSAGE_NO_SELECTED_FILE}), 400
 
     if file:
         # 파일 저장
@@ -35,34 +47,36 @@ def predict():
         results = model.predict(img)  # 이미지 객체를 사용하여 예측
 
         # 결과 처리 및 텍스트 파일로 저장
-        result_txt_path = os.path.join(app.config['UPLOAD_FOLDER'], 'result.txt')
+        result_txt_path = os.path.join(app.config['UPLOAD_FOLDER'], RESULT_TEXT_FILE)
         results[0].save_txt(result_txt_path)  # 첫 번째 결과를 텍스트 파일로 저장
 
-        # 텍스트 파일의 내용을 String으로 변환
+        # 텍스트 파일의 내용을 String으로 변환하고 \n을 공백으로 바꿈
         with open(result_txt_path, 'r') as f:
-            predictions_txt = f.read()
+            predictions_txt = f.read().replace('\n', ' ')
 
         # 스프링 서버로 예측 결과 전송
-        spring_server_url = 'https://' + DEV_SERVER_URL + '/endpoint'
-        response = requests.post(spring_server_url, json={'predictions_txt': predictions_txt})
+        spring_server_url = 'https://' + DEV_SERVER_URL + '/predict'
+        response = requests.post(spring_server_url, json={'predictions': predictions_txt})
 
         if response.status_code == 200:
-            return jsonify({'message': 'Prediction sent successfully', 'predictions': predictions_txt})
+            return jsonify({'message': SUCCESS_MESSAGE, 'predictions': predictions_txt})
         else:
             return jsonify({'error': 'Failed to send prediction to Spring server'}), 500
 
-    return jsonify({'error': 'File upload failed'}), 500
+    return jsonify({'error': ERROR_MESSAGE_FILE_UPLOAD_FAILED}), 500
 
 
 @app.route('/predict-test', methods=['POST'])
 def predict_test():
+
+    start_time = time.time()
     # 요청에서 파일 가져오기
     if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
+        return jsonify({'error': ERROR_MESSAGE_NO_FILE}), 400
 
     file = request.files['file']
     if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
+        return jsonify({'error': ERROR_MESSAGE_NO_SELECTED_FILE}), 400
 
     if file:
         # 파일 저장
@@ -74,21 +88,24 @@ def predict_test():
         results = model.predict(img)  # 이미지 객체를 사용하여 예측
 
         # 결과 처리 및 텍스트 파일로 저장
-        result_txt_path = os.path.join(app.config['UPLOAD_FOLDER'], 'result.txt')
+        result_txt_path = os.path.join(app.config['UPLOAD_FOLDER'], RESULT_TEXT_FILE)
         results[0].save_txt(result_txt_path)  # 첫 번째 결과를 텍스트 파일로 저장
 
-        # 텍스트 파일의 내용을 String으로 변환
+        # 텍스트 파일의 내용을 String으로 변환하고 \n을 공백으로 바꿈
         with open(result_txt_path, 'r') as f:
-            predictions_txt = f.read()
+            predictions_txt = f.read().replace('\n', ' ')
+
+            end_time = time.time()
+            print(f"Elapsed time: {end_time - start_time} seconds")
 
         return jsonify({'predictions': predictions_txt})
 
-    return jsonify({'error': 'File upload failed'}), 500
+    return jsonify({'error': ERROR_MESSAGE_FILE_UPLOAD_FAILED}), 500
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/health')
+def health_check():
+    return 'OK!'
 
 
 if __name__ == '__main__':
