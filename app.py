0304c1dab7356b709773b5b77d0a4c27d79c3d3b
Luke_YS <yjsmk0902@gmail.com>
2024-05-29 02:42:02 +0900

    :recycle: REFACTOR: predict 함수 수정
    - 자연어 처리 결과물 적재 해소

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 import os
 from ultralytics import YOLO
-import requests
 from PIL import Image
 
 # Flask 애플리케이션 초기화
@@ -42,14 +41,18 @@ def predict():
         # 예측 수행
         img = Image.open(file_path)
         results = model.predict(img)  # 이미지 객체를 사용하여 예측
+        os.remove(file_path)
 
         # 결과 처리 및 텍스트 파일로 저장
         result_txt_path = os.path.join(app.config['UPLOAD_FOLDER'], RESULT_TEXT_FILE)
         results[0].save_txt(result_txt_path)  # 첫 번째 결과를 텍스트 파일로 저장
+        results[0].save(filename='result.jpg')
 
         # 텍스트 파일의 내용을 String으로 변환하고 \n을 공백으로 바꿈
         with open(result_txt_path, 'r') as f:
             predictions_txt = f.read().replace('\n', ' ')
+
+        os.remove(result_txt_path)
 
         return predictions_txt
 
